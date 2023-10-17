129eb514640d6ee188d21da0ca51c61b0b83cc3a
StaProgramer <baranov.stas2020@gmail.com>
2023-10-17 21:02:28 +0300

    first commit

---
+++ FloatingPoint/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b;
+        char оператор;
+
+        Console.Write("Введiть перше число: ");
+        if (double.TryParse(Console.ReadLine(), out a))
+        {
+            Console.Write("Введiть операцiю (+, -, *, /): ");
+            оператор = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Введiть друге число: ");
+            if (double.TryParse(Console.ReadLine(), out b))
+            {
+                double результат = 0.0;
+
+                switch (оператор)
+                {
+                    case '+':
+                        результат = a + b;
+                        break;
+                    case '-':
+                        результат = a - b;
+                        break;
+                    case '*':
+                        результат = a * b;
+                        break;
+                    case '/':
+                        if (b != 0)
+                            результат = a / b;
+                        else
+                            Console.WriteLine("Помилка: Дiлення на нуль.");
+                        break;
+                    default:
+                        Console.WriteLine("Непiдтримувана операція.");
+                        break;
+                }
+
+                Console.WriteLine("Результат: " + результат);
+            }
+            else
+            {
+                Console.WriteLine("Некоректне друге число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некоректне перше число.");
+        }
+    }
+}
